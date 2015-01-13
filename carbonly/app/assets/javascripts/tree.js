55868fd5a9b4d43e85a9c71e72c412167afae1f3
Michael Foody <mfoody@hotmail.com>
2015-01-13 16:44:20 +1100

    new tree.js new tree svg file edited index to have trees display inline

--- carbonly/app/assets/javascripts/tree.js
+++ carbonly/app/assets/javascripts/tree.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 window.tree = {
   initialize: function() {
       $('.tree svg').each(function() {
@@ -40,3 +41,64 @@ function updateTrees () {
         window.tree.animate();
     }, 500);
 };
+=======
+// window.tree = {
+//   initialize: function() {
+//       $('.tree svg').each(function() {
+//           var delay, length, path, paths, previousStrokeLength, speed, _i, _len, _results;
+//           paths = $('path');
+//           delay = 0;
+//           _results = [];
+//           for (_i = 0, _len = paths.length; _i < _len; _i++) {
+//               path = paths[_i];
+//               length = path.getTotalLength();
+//               previousStrokeLength = speed || 0;
+//               speed = length < 100 ? 20 : Math.floor(length);
+//               delay += previousStrokeLength + 100;
+//               _results.push($(path).css('transition', 'none').attr('data-length', length).attr('data-speed', speed).attr('data-delay', delay).attr('stroke-dashoffset', length).attr('stroke-dasharray', length + ',' + length));
+//           }
+//           return _results;
+//       });
+//   },
+//   animate: function() {
+//       return $('.tree svg').each(function() {
+//           var delay, length, path, paths, speed, _i, _len, _results;
+//           paths = $('path');
+//           _results = [];
+//           for (_i = 0, _len = paths.length; _i < _len; _i++) {
+//               path = paths[_i];
+//               length = $(path).attr('data-length');
+//               speed = $(path).attr('data-speed');
+//               delay = $(path).attr('data-delay');
+//               _results.push($(path).css('transition', 'stroke-dashoffset ' + speed + 'ms ' + delay + 'ms linear').attr('stroke-dashoffset', '0'));
+//           }
+//           return _results;
+//       });
+//   } 
+// };
+// $(function() {
+//   //INITIALIZE AND ANIMATE TREE  
+//   window.tree.initialize();
+//   $('#button-submit').on('click', function() {
+//       window.tree.initialize();
+//       setTimeout(function() {
+//           window.tree.animate();
+//       }, 500);
+//   });
+// });
+$(function(){
+
+var hello = document.querySelector('#hello');
+var l = hello.getTotalLength();
+
+hello.style.strokeDasharray = l.toString() + ' ' + l.toString();
+hello.style.strokeDashoffset = l;
+
+hello.getBoundingClientRect();
+
+hello.style.opacity = 1;
+hello.style.strokeDashoffset = 0;
+
+
+});
+>>>>>>> new tree.js new tree svg file edited index to have trees display inline
