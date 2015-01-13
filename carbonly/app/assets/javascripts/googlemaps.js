37cc3e392446dc2c35f031064788a8773016d091
Michael Foody <mfoody@hotmail.com>
2015-01-13 23:53:36 +1100

    css tweaks

--- carbonly/app/assets/javascripts/googlemaps.js
+++ carbonly/app/assets/javascripts/googlemaps.js
@@ -7,8 +7,10 @@
       directionsDisplay = new google.maps.DirectionsRenderer();
       var chicago = new google.maps.LatLng(41.850033, -87.6500523);
       var mapOptions = {
+
         zoom:7,
         center: chicago
+
       };
       map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
       directionsDisplay.setMap(map);
