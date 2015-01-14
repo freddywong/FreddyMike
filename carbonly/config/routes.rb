eeaf9c6b72a637def5ce70a87a21b84a983dfb3c
Frederick Wong <frederick.kf.wong@gmail.com>
2015-01-14 14:59:49 +1100

    Devise generator has been created

--- carbonly/config/routes.rb
+++ carbonly/config/routes.rb
@@ -13,8 +13,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   resources :trips
-
-  root 'trips#index'
+  root "home#index"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
